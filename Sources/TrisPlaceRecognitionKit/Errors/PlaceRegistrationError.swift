5db03783be2ba38ea3551fcf7dffbeb07c96fc15
Tris <hbabnd@gmail.com>
2026-09-22 03:57:26 +0900

    feat - add place registration domain models

---
+++ Sources/TrisPlaceRecognitionKit/Errors/PlaceRegistrationError.swift
@@ -0,0 +1,13 @@
+//
+//  PlaceRegistrationError.swift
+//  TrisPlaceRecognitionKit
+//
+//  Created by COMATOKI on 2026-09-22.
+//
+
+public enum PlaceRegistrationError: Error,
+                                    Sendable,
+                                    Equatable {
+    case emptyName
+    case nameTooLong
+}
---
+++ Sources/TrisPlaceRecognitionKit/Models/PlaceLocation.swift
@@ -0,0 +1,25 @@
+//
+//  PlaceLocation.swift
+//  TrisPlaceRecognitionKit
+//
+//  Created by COMATOKI on 2026-09-22.
+//
+
+public struct PlaceLocation: Sendable,
+                             Equatable,
+                             Hashable {
+
+    public let latitude: Double
+    public let longitude: Double
+    public let recognitionRadius: Double
+
+    public init(
+        latitude: Double,
+        longitude: Double,
+        recognitionRadius: Double
+    ) {
+        self.latitude = latitude
+        self.longitude = longitude
+        self.recognitionRadius = recognitionRadius
+    }
+}
